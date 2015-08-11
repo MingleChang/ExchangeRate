45a810c6e68858846d5f2afb2108f0b4b88691d8
Mingle Chang <mingle890805@gmail.com>
2015-08-12 00:17:32 +0800

    not

--- ExchangeRate/Common/MCExchangeRateRequest.h
+++ ExchangeRate/Common/MCExchangeRateRequest.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-//http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22GBPUSD%22,%22%22,%22HKDUSD%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+//http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22GBPUSD%22,%22HKDUSD%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
 //select * from yahoo.finance.xchange where pair in ("EURUSD","GBPUSD")
 //{"query":{"count":2,"created":"2015-07-23T01:41:37Z","lang":"zh-cn","results":{"rate":[{"id":"EURUSD","Name":"EUR/USD","Rate":"1.0940","Date":"7/23/2015","Time":"2:41am","Ask":"1.0942","Bid":"1.0937"},{"id":"GBPUSD","Name":"GBP/USD","Rate":"1.5614","Date":"7/23/2015","Time":"2:41am","Ask":"1.5616","Bid":"1.5612"}]}}}
 @class MCExchangeRate;
--- ExchangeRate/Controller/MCCurrencyListViewController.m
+++ ExchangeRate/Controller/MCCurrencyListViewController.m
@@ -15,11 +15,10 @@
 
 @interface MCCurrencyListViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property(nonatomic,strong)UISearchBar *searchBar;
 
-@property(nonatomic,strong)NSMutableArray *selectedCurrencies;
-@property(nonatomic,strong)NSMutableArray *unselectedCurrencies;
-- (IBAction)leftBarButtonClick:(UIBarButtonItem *)sender;
-- (IBAction)rightBarButtonClick:(UIBarButtonItem *)sender;
+- (void)leftBarButtonClick:(UIBarButtonItem *)sender;
+- (void)rightBarButtonClick:(UIBarButtonItem *)sender;
 
 @end
 
@@ -42,157 +41,51 @@
 }
 -(void)initAllSubViews{
     [self.tableView registerNib:[UINib nibWithNibName:MCCurrencyListCellID bundle:nil] forCellReuseIdentifier:MCCurrencyListCellID];
-    self.tableView.editing=YES;
+    self.searchBar=[[UISearchBar alloc]init];
+    self.searchBar.barStyle=UIBarStyleBlackTranslucent;
+    self.navigationItem.titleView=self.searchBar;
+    UIBarButtonItem *lRightBarButtonItem=[[UIBarButtonItem alloc]initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(rightBarButtonClick:)];
+    self.navigationItem.rightBarButtonItem=lRightBarButtonItem;
 }
 -(void)initAllData{
-    self.selectedCurrencies=[[DataManager manager].selectedCurrencies mutableCopy];
-    self.unselectedCurrencies=[[DataManager manager].unselectedCurrencies mutableCopy];
 }
 #pragma mark - Event Response
-- (IBAction)leftBarButtonClick:(UIBarButtonItem *)sender {
-    if ([self.delegate respondsToSelector:@selector(currencyListViewControllerLeftBarButtonClick:)]) {
-        [self.delegate currencyListViewControllerLeftBarButtonClick:self];
-    }
-    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+- (void)leftBarButtonClick:(UIBarButtonItem *)sender {
 }
 
-- (IBAction)rightBarButtonClick:(UIBarButtonItem *)sender {
-    [DataManager manager].selectedCurrencies=[self.selectedCurrencies copy];
-    [[DataManager manager]saveSelectedCurrency];
-    if ([self.delegate respondsToSelector:@selector(currencyListViewControllerRightBarButtonClick:)]) {
-        [self.delegate currencyListViewControllerRightBarButtonClick:self];
+- (void)rightBarButtonClick:(UIBarButtonItem *)sender {
+    if ([self.searchBar isFirstResponder]) {
+        self.searchBar.text=@"";
+        [self.searchBar resignFirstResponder];
+    }else{
+        [self.navigationController dismissViewControllerAnimated:YES completion:nil];
     }
-    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 #pragma mark - TableView DataSource
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 2;
+    return 1;
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    if (section==0) {
-        return self.selectedCurrencies.count;
-    }else{
-        return self.unselectedCurrencies.count;
-    }
+    return [DataManager manager].allCurrencies.count;
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     MCCurrencyListCell *lCell=[tableView dequeueReusableCellWithIdentifier:MCCurrencyListCellID forIndexPath:indexPath];
     NSInteger row=[indexPath row];
-    NSInteger section=[indexPath section];
-    MCCurrency *lCurrency=nil;
-    if (section==0) {
-        lCurrency=[self.selectedCurrencies objectAtIndex:row];
-        lCell.backgroundColor=RGBA(0, 0, 0, 0.2);
-    }else{
-        lCurrency=[self.unselectedCurrencies objectAtIndex:row];
-        lCell.backgroundColor=[UIColor clearColor];
-    }
+    MCCurrency *lCurrency=[DataManager manager].allCurrencies[row];
     lCell.currency=lCurrency;
     [lCell updateUI];
     return lCell;
 }
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return YES;
-}
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if ([indexPath section] == 0) {
-        return YES;
-    }else{
-        return NO;
-    }
-}
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (indexPath == nil) {//5s 7.1 点快了会传回nil
-        return;
-    }
-    NSInteger row=[indexPath row];
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        MCCurrency *lCurrency=[self.selectedCurrencies objectAtIndex:row];
-        [self.selectedCurrencies removeObjectAtIndex:row];
-        [tableView beginUpdates];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-        [tableView endUpdates];
-        
-        [self.unselectedCurrencies addObject:lCurrency];
-        [tableView beginUpdates];
-        [tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.unselectedCurrencies.count - 1 inSection:1]] withRowAnimation:UITableViewRowAnimationAutomatic];
-        [tableView endUpdates];
-        
-        if (self.unselectedCurrencies.count==1) {
-            [tableView reloadSections:[[NSIndexSet alloc] initWithIndex:1] withRowAnimation:UITableViewRowAnimationAutomatic];
-        }
-    }else if (editingStyle == UITableViewCellEditingStyleInsert)
-    {
-        MCCurrency *lCurrency=[self.unselectedCurrencies objectAtIndex:row];
-        [self.unselectedCurrencies removeObjectAtIndex:row];
-        [tableView beginUpdates];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-        [tableView endUpdates];
-        
-        [self.selectedCurrencies addObject:lCurrency];
-        [tableView beginUpdates];
-        [tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.selectedCurrencies.count - 1 inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
-        [tableView endUpdates];
-        
-        if (self.unselectedCurrencies.count == 0) {
-            [tableView reloadSections:[[NSIndexSet alloc] initWithIndex:1] withRowAnimation:UITableViewRowAnimationAutomatic];
-        }
-    }
-}
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
-{
-    
-    [self.selectedCurrencies exchangeObjectAtIndex:sourceIndexPath.row withObjectAtIndex:destinationIndexPath.row];
-}
+
 #pragma mark - TableView Delegate
--(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    if (section == 1) {
-        return 44;
-    }
-    return 0;
-}
+
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     return 44;
 }
-
-
--(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
-    if (section==1) {
-        UIView *lView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 44)];
-        lView.backgroundColor=[UIColor clearColor];
-        UIView *lLineView=[[UIView alloc]initWithFrame:CGRectMake(0,44-ONE_PIXEL, tableView.frame.size.width, ONE_PIXEL)];
-        lLineView.backgroundColor=RGB(137, 137, 137);
-        [lView addSubview:lLineView];
-        UILabel *lLabel=[[UILabel alloc]initWithFrame:CGRectMake(13, 44-25, tableView.frame.size.width, 20)];
-        lLabel.textColor=[UIColor whiteColor];
-        lLabel.font=[UIFont systemFontOfSize:15];
-        lLabel.text=@"未选择货币";
-        [lView addSubview:lLabel];
-        return lView;
-    }else{
-        return nil;
-    }
-}
-
--(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if ([indexPath section]==0) {
-        return UITableViewCellEditingStyleDelete;
-    }else{
-        return UITableViewCellEditingStyleInsert;
-    }
-}
-- (NSIndexPath *)tableView:(UITableView *)tableView targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath
-{
-    if (proposedDestinationIndexPath.section == 1) {
-        return sourceIndexPath;
-    }else{
-        return proposedDestinationIndexPath;
-    }
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 /*
 #pragma mark - Navigation
